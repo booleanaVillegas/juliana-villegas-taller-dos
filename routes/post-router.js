4a6edabbdecaee184d6940f21e97e52abd67fb4e
Jhon <albeosorioz88x@gmail.com>
2017-04-25 20:33:11 -0500

    "Sube" imágenes

--- routes/post-router.js
+++ routes/post-router.js
@@ -3,6 +3,37 @@ var router = express.Router();
 var controller = require('../controllers/post-controller');
 var likeController = require('../controllers/like-controller');
 var commentController = require('../controllers/comment-controller');
+var multer = require('multer');
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+});
+
+var upload = multer({ storage: storage }).single('profileImage');
+
+
+router.post('/', function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            // An error occurred when uploading
+        }
+        res.json({
+            success: true,
+            message: 'Image uploaded!'
+        });
+
+        // Everything went fine
+    })
+});
+
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
