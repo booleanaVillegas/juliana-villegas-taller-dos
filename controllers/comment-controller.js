bc14f5a2ae02ead4c59999e4b00d840241ff50f7
booleanaVillegas <jumavipe@hotmail.com>
2017-04-26 23:35:16 -0500

    crea comments con postman

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,14 +1,30 @@
 var db = require('../database');
 
-exports.ObtenerPorPost = function(idpost,callback){
+exports.ObtenerPorPost = function(idpost,callback) {
 
-    db.getConexion().query('SELECT * FROM comments WHERE id_post=?',idpost, function(err, results){
-        if(!err){
+    db.getConexion().query('SELECT * FROM comments WHERE id_post=?', idpost, function (err, results) {
+        if (!err) {
             console.log("");
             //console.log(results);
             callback(false, results);
-        }else{
+        } else {
             callback(err);
         }
     });
 }
+
+    exports.nuevoComment = function(contenido,idpost,username,callback){
+
+        var values = [contenido,username,idpost];
+        db.getConexion().query('INSERT INTO comments (content,username,id_post) VALUES (?,?,?)',
+            values,
+            function(err, result){
+                if(err){
+                    callback(err);
+                }else{
+                    callback(false, result.insertId);
+                }
+            }
+        );
+
+    }
